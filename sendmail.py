554e1deabc208b0c4eec73314f8b30ba667feb3b
Sai Charan <saicharankmrs@gmail.com>
2022-10-17 00:36:01 +0530

    change_vja_16-10-2022

--- sendmail.py
+++ sendmail.py
@@ -1,9 +1,10 @@
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com',587)
-
 server.starttls()
-
 server.login('kanbaniitm@gmail.com','tsslafciqjfvffgz')
 
-server.sendmail('kanbaniitm@gmail.com','saicharankmrs@gmail.com','<b>Mail From Python!!</b>')
-print("mailsent")
+def mail_otp(mailid):
+    import random as r
+    otp = (r.randint(1111,9999))
+    server.sendmail('kanbaniitm@gmail.com',mailid,'OTP for KanbanApp:'+otp+"\n\nValid for 10 Mins.")
+    return otp
